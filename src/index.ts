6fcf01e633451c57cc851b56a8437fd519ccfbb4
Nicolas Thompson <mail@nicolasthompson.com>
2024-04-23 09:16:21 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,5 @@ export * from './events/base-publisher';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/types/order-status';
+export * from './events/order-cancelled-event';
+export * from './events/order-created-event';
